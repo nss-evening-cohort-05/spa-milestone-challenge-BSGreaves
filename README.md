44eed0e7d7709f5303019d63e6bde39d359df237
Ben Greaves <bsgreaves@gmail.com>
2017-03-22 20:17:06 -0500

    Readme Done. Ready for feedback.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# spa-milestone-challenge
+# Mazda Carlot
+##Single Page Applications Milestone Challenge
+
+For this Nashville Software School challenge, we were asked to mock-up a car dealership website.
+
+The car data is loaded into a private array and then printed to the DOM. When the user clicks a card, that card is visually selected and the user may edit the car's description using the input field in the nav bar. When the user hits enter, the description and any edits are saved to the card, and the card is deselected. This exercise was designed as a test of my bootstrap and vanilla javascript DOM manipulation skills.
+
+## Technologies used
+HTML, CSS, Bootstrap, Javascript, XHR
+
+## Installation
+
+```
+git clone git@github.com:nss-evening-cohort-05/spa-milestone-challenge-BSGreaves.git
+cd nss-evening-cohort-05/spa-milestone-challenge-BSGreaves
+npm install http-server -g
+http-server -p 8080
+```
+
+In your browser, navigate to:
+`http://localhost:8080`
+
+## Contributors
+
+[Ben Greaves](https://github.com/BSGreaves)
--- events.js
+++ events.js
@@ -1,34 +1,34 @@
-var Carlot = (function(oldCarlot){
+var Carlot = (function(oldCarlot) {
 
-	var cardSelected = false; //flag to prevent multiple card selections
-	var descriptionEditor = document.querySelector("#inventoryEdit");
-	var targetDescription;
+    var cardSelected = false; //flag to prevent multiple card selections
+    var descriptionEditor = document.querySelector("#inventoryEdit");
+    var targetDescription;
 
-	oldCarlot.addEventListeners = function(cardArray){
-		cardArray.forEach(card => card.addEventListener("click", function(e){
-			if (cardSelected === false) {
-				let targetCard = e.target;
-				while (targetCard.classList.contains("car-card") === false) { //gets parent element car-card
-					targetCard = targetCard.parentNode;
-				}
-				cardSelected = true;
-				Carlot.styleSelectedCard(targetCard, "#eaf2ff"); //styles the card
-				descriptionEditor.focus(); 
-				targetDescription = targetCard.children[1].children[2];
-				descriptionEditor.value = targetDescription.innerText;
-				descriptionEditor.addEventListener("keyup", Carlot.pairDescription); //pairs description
-			}
-		}));
-	};
-	oldCarlot.pairDescription = function(e){
-		targetDescription.innerText = descriptionEditor.value;
-		if (e.keyCode === 13) {
-			targetDescription.innerText = descriptionEditor.value;
-	        descriptionEditor.value = "";
-	        descriptionEditor.removeEventListener("keyup", Carlot.pairDescription);
-	        cardSelected = false; //frees the main event listener
-	        Carlot.revertCard(); //reverts styles to normal
-	    }
-	};
-	return oldCarlot;
+    oldCarlot.addEventListeners = function(cardArray) {
+        cardArray.forEach(card => card.addEventListener("click", function(e) {
+            if (cardSelected === false) {
+                let targetCard = e.target;
+                while (targetCard.classList.contains("car-card") === false) { //gets parent element car-card
+                    targetCard = targetCard.parentNode;
+                }
+                cardSelected = true;
+                Carlot.styleSelectedCard(targetCard, "#eaf2ff"); //styles the card
+                descriptionEditor.focus();
+                targetDescription = targetCard.children[1].children[2];
+                descriptionEditor.value = targetDescription.innerText;
+                descriptionEditor.addEventListener("keyup", Carlot.pairDescription); //pairs description
+            }
+        }));
+    };
+    oldCarlot.pairDescription = function(e) {
+        targetDescription.innerText = descriptionEditor.value;
+        if (e.keyCode === 13) {
+            targetDescription.innerText = descriptionEditor.value;
+            descriptionEditor.value = "";
+            descriptionEditor.removeEventListener("keyup", Carlot.pairDescription);
+            cardSelected = false; //frees the main event listener
+            Carlot.revertCard(); //reverts styles to normal
+        }
+    };
+    return oldCarlot;
 })(Carlot || {})
